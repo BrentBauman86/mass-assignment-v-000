87a0c8a8cfb7393b9436d873ad45f5eb69333090
Brent Bauman <brentbauman86@gmail.com>
2018-07-17 16:26:42 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,20 @@
+require "pry"
 class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
   :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
+# binding.pry
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
+
+def beerbottle
+  puts "*********************************"
+end
+
+
+def self.create(attributes_hash)
+  attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+end 
